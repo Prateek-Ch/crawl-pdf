6eb912acafde3d3041c6b4e15f9b5958e00e6bfc
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2026-03-17 16:58:17 +0530

    Initial commit

---
+++ src/crawlers/base_crawler.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class BaseCrawler(ABC):
+
+    def __init__(self, topic, max_docs=100):
+        self.topic = topic
+        self.max_docs = max_docs
+
+    @abstractmethod
+    def fetch_pdf_links(self):
+        pass
